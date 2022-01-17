05424755e8cdab7425900aa681c03276282f69bb
uyvn <97893960+uyvn@users.noreply.github.com>
2022-01-17 08:57:28 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+// get users IP
+function getIP() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+// grab ip info
+$ip_info = file_get_contents("http://www.ip-api.com/line/".getIP());
+
+// log ip
+$myfile = fopen("iplogs.txt", "w") or die("Failed to open logs file.");
+fwrite($myfile, "====================\n".$ip_info."\n====================\n");
+fclose($myfile);
+
+?>
